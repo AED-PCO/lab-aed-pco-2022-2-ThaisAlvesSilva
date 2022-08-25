798cdef6a6e7c97769956b6ef54d5dacb4768f27
Thais Alves <alvesthais406@gmail.com>
2022-08-25 16:45:21 -0300

    Exercicio Fibonacci

---
+++ codigo/Laboratório2/fibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Digite a posição da sequencia Fibonacci que deseja saber o valor:");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O {num}º número da sequencia de Fibonacci é o: {fib(num)}");
+        }
+
+        static int fib(int x){
+            if( x == 1)
+                return 0;
+            if( x == 2)
+                return 1;
+
+            return fib(x-1) + fib(x -2);
+        }
+    }
+}
