314e66fec22f7ac86ba2cdad583261924739acb8
Thais Alves <alvesthais406@gmail.com>
2022-09-15 15:34:27 -0300

    Exercicios Busca Binária

---
+++ codigo/Laboratório3/buscaBinaria1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace buscaBinaria1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vet = new int[5];
+            int num,pos;
+
+            preencheVetor(vet);
+
+            Console.WriteLine("Valores do Vetor:");
+            imprimeVetor(vet);
+
+            Console.WriteLine("\n\nDigite o valor que deseja buscar no vetor:");
+            num = int.Parse(Console.ReadLine());
+
+            pos = buscaBinariaRecursiva(vet,num,0, (vet.Length -1));
+
+            if(pos != -1)
+                Console.WriteLine($"O número {num} está na posição {pos} do vetor.");
+            else
+               Console.WriteLine($"O número {num} não está no vetor."); 
+        }
+
+        static int buscaBinariaRecursiva(int[] vet, int num, int posInicial,int posFinal){
+            if(posFinal < posInicial)
+                return -1;      //NUMERO NÃO EXISTE NO VETOR
+            
+            int posMeio = (posFinal + posInicial) / 2;
+            if(vet[posMeio] < num)
+                return  buscaBinariaRecursiva(vet,num,(posMeio + 1), posFinal);
+            else if(vet[posMeio] > num)
+                return  buscaBinariaRecursiva(vet,num, posInicial,(posMeio - 1));
+            else
+                return posMeio;
+            
+        }
+
+        static void selectionSort(int[] vet){
+            int min, aux;
+
+            for(int i=0; i<vet.Length; i++){
+                min = i;
+
+                for(int j = i+1; j < vet.Length; j++){
+                    if(vet[j] < vet[min]){
+                        min = j;
+                    }
+                }
+
+                if(min != i){
+                    aux = vet[min];
+                    vet[min] = vet[i];
+                    vet[i] = aux;
+                }
+            }
+        }
+
+        static void preencheVetor(int[] vet){
+            Random randNum = new Random();
+            for(int i = 0; i < vet.Length; i++){
+                vet[i] = i+2;
+            }
+        }
+
+        static void imprimeVetor(int[] vet){
+            for(int i = 0; i < vet.Length; i++){
+                Console.Write("\t" + vet[i]);
+            }
+        }
+    }
+}
---
+++ codigo/Laboratório3/buscaBinaria2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace buscaBinariaRecursivaSemPos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vet = new int[5];
+            int num,pos;
+
+            preencheVetor(vet);
+
+            Console.WriteLine("\nValores do vetor:");
+            imprimeVetor(vet);
+
+            Console.WriteLine("\n\nDigite o valor que deseja buscar no vetor:");
+            num = int.Parse(Console.ReadLine());
+
+            pos = buscaBinariaRecursivaSemPos(vet,num);
+
+            if(pos != -1)
+                Console.WriteLine($"O número {num} está no vetor.");
+            else
+               Console.WriteLine($"O número {num} não está no vetor."); 
+        }
+
+        static int buscaBinariaRecursivaSemPos(int[] vet, int num){  
+            if(vet.Length == 0){
+                return -1;         //NUMERO NÃO EXISTE NO VETOR
+            }
+            
+            int posMeio = vet.Length / 2;
+
+            if(vet[posMeio] < num){
+                int[] vetAux = new int[vet.Length - (posMeio +1)];
+                Array.Copy(vet, posMeio+1,vetAux,0,vet.Length - (posMeio+1));       
+                
+                return  buscaBinariaRecursivaSemPos(vetAux,num);
+            }else if(vet[posMeio] > num){
+                int[] vetAux = new int[posMeio];
+                Array.Copy(vet,0,vetAux,0,posMeio);         // primeiro 0: vai pegar a partir da posição 0, posMeio: quantas posições vai pegar
+                
+                return  buscaBinariaRecursivaSemPos(vetAux,num);    
+            }
+            else
+                return 1;
+            
+        }
+
+        static void preencheVetor(int[] vet){
+            Random randNum = new Random();
+            for(int i = 0; i < vet.Length; i++){
+                vet[i] = i+ 2;
+            }
+        }
+
+        static void imprimeVetor(int[] vet){
+            for(int i = 0; i < vet.Length; i++){
+                Console.Write("\t" + vet[i]);
+            }
+        }
+    }
+}
